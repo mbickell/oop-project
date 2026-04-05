e5a2309d4084582156abfe9c6b52104e5d62adcd
Matthew Bickell <matthewbickell@hotmail.com>
2026-04-05 17:11:31 +0300

    feat: blank slate

--- src/oopproject/Controller.java
+++ src/oopproject/Controller.java
@@ -1,16 +1,12 @@
 package oopproject;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
 public class Controller {
 
   @FXML
-  private Label label;
 
   public void initialize() {
-    String javaVersion = System.getProperty("java.version");
-    String javafxVersion = System.getProperty("javafx.version");
-    label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+
   }
 }
--- src/oopproject/Main.java
+++ src/oopproject/Main.java
@@ -10,8 +10,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("oop-project.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("School Library");
         primaryStage.setScene(new Scene(root, 400, 300));
         primaryStage.show();
     }
