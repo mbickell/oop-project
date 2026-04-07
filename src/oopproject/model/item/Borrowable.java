603627e01dda5656c768fb066d67134a28fe02b3
Matthew Bickell <matthewbickell@hotmail.com>
2026-04-07 11:21:06 +0300

    feat: add abstarct library item class

---
+++ src/oopproject/model/item/Borrowable.java
@@ -0,0 +1,9 @@
+package oopproject.model.item;
+
+public interface Borrowable {
+  public int getBorrowingDays();
+
+  public String getType();
+
+  public String getDisplayInfo();
+}
---
+++ src/oopproject/model/item/LibraryItem.java
@@ -0,0 +1,34 @@
+package oopproject.model.item;
+
+public abstract class LibraryItem implements Borrowable {
+  private String itemID;
+  private String title;
+  private boolean isAvailable;
+
+  LibraryItem(String itemID, String title) {
+    this.itemID = itemID;
+    this.title = title;
+    this.isAvailable = true;
+  }
+
+  public String getItemID() {
+    return itemID;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public boolean getIsAvailable() {
+    return isAvailable;
+  }
+
+  public void setAvailable(boolean isAvailable) {
+    this.isAvailable = isAvailable;
+  }
+
+  @Override
+  public String toString() {
+    return "[" + getType() + "]" + " " + this.title + (this.isAvailable ? "" : " (Borrowed)");
+  }
+}
