32fbf0663241a77ba7c3a50284fd81b36886d747
Matthew Bickell <matthewbickell@hotmail.com>
2026-04-07 07:59:22 +0300

    home work

--- src/oopproject/Controller.java
+++ src/oopproject/Controller.java
@@ -1,10 +1,21 @@
 package oopproject;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 public class Controller {
 
   @FXML
+
+  ListView<String> itemListView;
+  Label itemInfoLabel;
+  ComboBox<String> userComboBox;
+  Label userInfoLabel;
+  Button borrowButton;
+  Button returnButton;
 
   public void initialize() {
 
