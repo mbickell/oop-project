c80ea46805698680d3ec7f1dc26ca8632668b4dc
Matthew Bickell <matthewbickell@hotmail.com>
2026-04-08 13:00:29 +0300

    feat: add user abstract class

---
+++ src/oopproject/model/user/LibraryUser.java
@@ -0,0 +1,68 @@
+package oopproject.model.user;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import oopproject.model.item.LibraryItem;
+
+abstract public class LibraryUser {
+  private String userID;
+  private String name;
+  private List<LibraryItem> borrowedItems = new ArrayList<LibraryItem>();
+
+  abstract public int getMaxBorrowable();
+
+  abstract public String getUserType();
+
+  public String getUserID() {
+    return userID;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public List<LibraryItem> getBorrowedItems() {
+    return Collections.unmodifiableList(borrowedItems);
+  }
+
+  public boolean canBorrow() {
+    return borrowedItems.size() < getMaxBorrowable();
+  }
+
+  public boolean borrowItem(LibraryItem item) {
+    if (canBorrow()) {
+      borrowedItems.add(item);
+      item.setAvailable(false);
+      return true;
+    }
+
+    return false;
+  }
+
+  public boolean returnItem(LibraryItem item) {
+    if (borrowedItems.size() <= 0 || item.getIsAvailable()) {
+      return false;
+    }
+
+    int borrowedBookIndex = 0;
+    ;
+
+    for (int i = 0; i < borrowedItems.size(); i++) {
+      if (borrowedItems.get(i).getItemID().compareTo(item.getItemID()) == 0) {
+        borrowedBookIndex = i;
+      }
+    }
+
+    borrowedItems.remove(borrowedBookIndex);
+    item.setAvailable(true);
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return getUserType() + ": " + getName() + " (limit: " + getMaxBorrowable() + ")";
+  }
+
+}
